f3d59950931fb583abbd87ca135c75951a4fb56c
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-05-23 22:38:37 +0200

    Wrote a for loop and created a new div element

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // Get the container element
 const container = document.getElementById("gridContainer");
+
+// create 256 divs (16x16)
+for(let i = 0; i < 256; i++) {
+    // create a new div element
+    const gridSquare = document.createElement("div")
+}
