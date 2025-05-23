30508e56a8347fe14d618a29eecff6d4e333e3d6
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-05-23 22:48:52 +0200

    Added click event for interactivity

--- script.js
+++ script.js
@@ -14,4 +14,10 @@ for(let i = 0; i < 256; i++) {
     const row = Math.floor(i / 16) + 1
     const col = (i % 16) + 1
     gridSquare.setAttribute("data-position", `${row}-${col}`)
+
+    // Add click event for interactivity
+    gridSquare.addEventListener("Click", function () {
+        this.style.backgroundColor = this.style.backgroundColor === "rgb(40, 167, 69)" ? "fff" :
+         "#28a745"
+    })
 }
